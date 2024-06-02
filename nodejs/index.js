9849f5055dcaaf97f547ad3659c5043885c9566b
r3yz0n <reyzon.thapa2020@gmail.com>
2024-06-02 11:57:10 +0545

    create server

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+//  Connect to mongoDBs
+const MONGODB_URI = "mongodb+srv://admin:admin@cluster0.3oldbww.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+mongoose.connect(MONGODB_URI).then(()=> {
+    console.log("Mongodb connect vayo...");
+}).catch((err)=> {
+    console.log("Connect vayena...", err);
+})
+
 
 app.get("/", (req, res) => {
     res.send("Hello nepal123!")
@@ -10,5 +21,3 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
     console.log("Server is running....")
 })
-
-// sqlite
