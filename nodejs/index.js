3344b6861d0993f0511fddb147341581b1badfbb
r3yz0n <reyzon.thapa2020@gmail.com>
2024-06-02 14:27:20 +0545

    add views and models

--- nodejs/index.js
+++ nodejs/index.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 //  Connect to mongoDBs
 const MONGODB_URI = "mongodb+srv://admin:admin@cluster0.3oldbww.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
@@ -16,6 +18,18 @@ mongoose.connect(MONGODB_URI).then(()=> {
 app.get("/", (req, res) => {
     res.send("Hello nepal123!")
 })
+
+// Get all books
+app.get("/allBooks", (req,res) => {
+    res.render("allBooks", {})
+})
+
+// Add new book
+app.get("/addBook", (req,res)=> {
+    res.render("addBook",{})
+})
+
+
 
 //  Creating a server on PORT 3000
 app.listen(3000, () => {
